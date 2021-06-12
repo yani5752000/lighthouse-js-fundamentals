9a2cd12e745f525c082f492db6f92f1a5f239e7f
Farid Asadpour <yani5752000@yahoo.com>
2021-06-12 18:38:09 +0000

    loopy-lighthouse.js

---
+++ loopy-lighthouse.js
@@ -0,0 +1,14 @@
+for(var x = 100; x <= 200; x++){
+  if(x % 12 == 0){
+    console.log("LoopyLighthouse");
+  }
+  else if(x % 3 == 0){
+    console.log("Loopy");
+  }
+  else if(x % 4 == 0){
+    console.log( "Lighthouse");
+  }
+  else{
+    console.log(x);
+  }
+}
