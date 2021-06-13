b3d0f5cef0c76de711fe922456b195913604068b
Farid Asadpour <yani5752000@yahoo.com>
2021-06-13 00:01:16 +0000

    last index

---
+++ lastIndexOf.js
@@ -0,0 +1,16 @@
+const lastIndexOf = function(array, value){
+  let num = -1;
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === value){
+      num = i;
+    }
+  }
+  return num;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
